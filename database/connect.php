e18d8cf6e71d594bd7d32d2996c61f19621575af
Kati Perkiö <142210484+katiperkio@users.noreply.github.com>
2024-12-16 05:28:09 +0200

    essentials

--- database/connect.php
+++ database/connect.php
@@ -1,8 +1,11 @@
 <?php
-$user = "root";
-$password = "";
-$host = "127.0.0.1";
-$database = "plushee_palz";
-$port = 3306;
 
-$connection = mysqli_connect($host, $user, $password, $database, $port);
+require dirname(DIR) . "/vendor/autoload.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable(DIR . '/..');
+$dotenv->safeLoad();
+$environment = $_ENV['ENVIRONMENT'];
+
+$connection = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_NAME']);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+mysqli_set_charset($connection, "utf8mb4"); // To set UTF-8 formatting to the content
